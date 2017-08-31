51d3a0b3f4a4d8a65b67b587f345bae1935ac679
MertensMing <13680543450@163.com>
2017-08-31 23:52:53 +0800

    update

--- app/extend/helper.js
+++ app/extend/helper.js
@@ -4,23 +4,22 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  formatUrl(url, versionPath) {
+  formatJsUrl(url) {
     const env = this.app.env;
-    const versionStr = fs.readFileSync(versionPath, 'utf-8');
+    const versionStr = fs.readFileSync(path.resolve(__dirname, '../../version.js.json'), 'utf-8');
     const version = JSON.parse(versionStr);
     if (env === 'local') {
       return `http://ov9z0zlev.bkt.clouddn.com/${url}`;
     }
     return `http://ov9z0zlev.bkt.clouddn.com/${version[url]}`;
   },
-  formatJsUrl(url) {
-    return this.formatUrl(
-      url,
-      path.resolve(__dirname, '../../version.js.json'));
-  },
   formatCssUrl(url) {
-    return this.formatUrl(
-      `style/${url}`,
-      path.resolve(__dirname, '../../version.css.json'));
+    const env = this.app.env;
+    const versionStr = fs.readFileSync(path.resolve(__dirname, '../../version.css.json'), 'utf-8');
+    const version = JSON.parse(versionStr);
+    if (env === 'local') {
+      return `http://ov9z0zlev.bkt.clouddn.com/style/${url}`;
+    }
+    return `http://ov9z0zlev.bkt.clouddn.com/${version[url]}`;
   },
 };
